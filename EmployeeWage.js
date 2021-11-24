2be023cd2f54787226aae1b4bbd02e9ca66c7b63
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-24 20:02:41 +0530

    Added: Functioning on Ability to calculate daily employee wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,7 +1,21 @@
-const IS_ABSENT = 0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("UC1 - Employee is Absent.");
-}else{
-    console.log("UC1 - Employee is PRESENT");
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOURS = 20;
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck){
+    case IS_PART_TIME: 
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;    
 }
+
+let empWage = empHrs * WAGE_PER_HOURS;
+console.log("Emp Wage: " + empWage);
