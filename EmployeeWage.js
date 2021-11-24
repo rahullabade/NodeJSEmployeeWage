543deef867688e7063143c880773bfeb86d47472
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-24 19:30:03 +0530

    Added: program on NodeJs Learning Practices

---
+++ EmployeeWage.js
@@ -0,0 +1,7 @@
+const IS_ABSENT = 0
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if (empCheck == IS_ABSENT) {
+    console.log("UC1 - Employee is Absent.");
+}else{
+    console.log("UC1 - Employee is PRESENT");
+}
