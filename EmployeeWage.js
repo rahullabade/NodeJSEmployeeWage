b02caa13d3623fd69591d364073116df4a4c0b72
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-25 07:28:17 +0530

    Refactor: Functioning on calculating wages till num of working dats

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,7 +4,8 @@
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOURS = 20;
-    const NUM_OF_WORKING_DAYS = 2;
+    const NUM_OF_WORKING_DAYS = 10;
+    const MAX_HRS_IN_MONTH = 100;
     function getWorkingHours(empCheck) {
         switch (empCheck) {
             case IS_PART_TIME:
@@ -16,10 +17,14 @@
         }
     }
     let totalEmpHrs = 0;
-    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
-        let empCheck = Math.floor(Math.random() * 10) % 3;
+    let totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10 % 3);
         totalEmpHrs += getWorkingHours(empCheck);
     }
     let empWage = totalEmpHrs + WAGE_PER_HOURS;
-    console.log("Hours: " + totalEmpHrs + " Emp Wage: " + empWage);
+    console.log("Total Days: " + totalWorkingDays +
+                " Total Hrs: " + totalEmpHrs + " Emp Wage: " + empWage);
+    console.log("Hours: " + totalEmpHrs + " Emp Wage: " + empWage)
 }
