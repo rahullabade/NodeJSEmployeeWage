992d0bbc9ddf5862d958a9cfc6b445645d10368e
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-25 07:24:41 +0530

    Refactor: Functioning on calculatng wage for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,7 +4,7 @@
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOURS = 20;
-
+    const NUM_OF_WORKING_DAYS = 2;
     function getWorkingHours(empCheck) {
         switch (empCheck) {
             case IS_PART_TIME:
@@ -15,8 +15,11 @@
                 return 0;
         }
     }
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    let empWage = empHrs + WAGE_PER_HOURS;
-    console.log("Hours " + empHrs + " Emp Wage: " + empWage);
+    let totalEmpHrs = 0;
+    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs + WAGE_PER_HOURS;
+    console.log("Hours: " + totalEmpHrs + " Emp Wage: " + empWage);
 }
